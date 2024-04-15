ed52d66efe9f0286e27fc7dd41cc42eb7c3d43c0
Brandonlarsencode <brandon.satan.larsen@gmail.com>
2024-04-15 19:26:46 +0200

    commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,28 +1,35 @@
+import React from "react";
 import { Html, useProgress } from "@react-three/drei";
 
 const CanvasLoader = () => {
   const { progress } = useProgress();
+
   return (
     <Html
       as='div'
       center
       style={{
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
         flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
       }}
     >
-      <span className='canvas-loader'></span>
+      <div style={{ marginBottom: 10 }}>
+        {/* Adjust size and style of the loading spinner */}
+        <div className='canvas-loader' style={{ width: 40, height: 40 }} />
+      </div>
       <p
         style={{
           fontSize: 14,
           color: "#F1F1F1",
           fontWeight: 800,
-          marginTop: 40,
+          margin: 0,
         }}
       >
-        {progress.toFixed(2)}%
+        {/* Round progress to integer percentage */}
+        {Math.round(progress * 100)}%
       </p>
     </Html>
   );
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,6 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
+      {/* Lights and scene setup */}
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
@@ -19,6 +20,8 @@ const Computers = ({ isMobile }) => {
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
+
+      {/* Computer model */}
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
@@ -33,21 +36,15 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
@@ -56,16 +53,17 @@ const ComputersCanvas = () => {
   return (
     <Canvas
       frameloop='demand'
-      shadows
+      shadows={isMobile ? false : true} // Disable shadows on mobile
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
-          enableZoom={false}
+          enableZoom={!isMobile} // Disable zoom on mobile
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          enableDamping={!isMobile} // Enable damping for smoother controls on desktop
         />
         <Computers isMobile={isMobile} />
       </Suspense>

c1b2cdb48c78da99159aad3c9eede6e616b16aff
Brandonlarsencode <brandon.satan.larsen@gmail.com>
2024-04-15 15:54:46 +0200

    commit all

---
+++ src/react-dom-utils.js
@@ -0,0 +1,7 @@
+// react-dom-utils.js
+
+export function createRoot(container) {
+    // Implement the createRoot function logic here
+  }
+  
+  createRoot(document.getElementById("root")).render(<App />);

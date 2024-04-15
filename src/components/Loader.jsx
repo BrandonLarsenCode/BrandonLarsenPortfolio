6e8efa6abb0c935a3c0036605d7fccb76d006494
Brandonlarsencode <brandon.satan.larsen@gmail.com>
2024-04-15 23:38:13 +0200

    commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,35 +1,28 @@
-import React from "react";
 import { Html, useProgress } from "@react-three/drei";
 
 const CanvasLoader = () => {
   const { progress } = useProgress();
-
   return (
     <Html
       as='div'
       center
       style={{
         display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
         justifyContent: "center",
-        height: "100%",
+        alignItems: "center",
+        flexDirection: "column",
       }}
     >
-      <div style={{ marginBottom: 10 }}>
-        {/* Adjust size and style of the loading spinner */}
-        <div className='canvas-loader' style={{ width: 40, height: 40 }} />
-      </div>
+      <span className='canvas-loader'></span>
       <p
         style={{
           fontSize: 14,
           color: "#F1F1F1",
           fontWeight: 800,
-          margin: 0,
+          marginTop: 40,
         }}
       >
-        {/* Round progress to integer percentage */}
-        {Math.round(progress * 100)}%
+        {progress.toFixed(2)}%
       </p>
     </Html>
   );
